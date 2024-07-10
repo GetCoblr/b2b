e14859f31c1df87828f17814403c75de63d5bf66
Uzair Nadeem <uzair@coblrshop.com>
2024-07-10 09:28:38 +0500

    Fixed failing build.

--- src/pages/support.js
+++ src/pages/support.js
@@ -1,15 +1,17 @@
 import * as React from "react";
 import Layout from "../components/Layout";
 import Header from "../components/support/Header";
-import FaqComponent from "../components/support/faqComponent";
 import TrailComponent from "../components/TrailComponent";
+import FaqComponent from "../components/support/FaqComponent";
 
 const SupportPage = () => {
-  return <Layout bg={true}>
-    <Header />
-    <FaqComponent />
-    <TrailComponent />
-  </Layout>;
+  return (
+    <Layout bg={true}>
+      <Header />
+      <FaqComponent />
+      <TrailComponent />
+    </Layout>
+  );
 };
 
 export default SupportPage;
